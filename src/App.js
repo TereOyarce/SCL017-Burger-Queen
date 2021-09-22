b470e351c1ab65ce8d6f3633ce5afd25b52f69b7
TeresaOyarce <t.oyarce05@gmail.com>
2021-09-22 09:58:37 -0300

    Root react , css index

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Cooking } from "./components/Cooking";
+import { Inicio } from "./components/Inicio";
+import { Menu } from "./components/Menu";
+import { Ready } from "./components/ListOrder";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="container" >
+      </div>
+      <Switch>
+
+        <Route path='/menu'>
+          <Menu />
+        </Route>
+        <Route path='/cooking'>
+          <Cooking />
+        </Route>
+        <Route path='/ready'>
+          <Ready/>
+        </Route>
+        <Route path='/'>
+          <Inicio />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Menu = () => {
+    return (
+        <div>
+            <h1>Tomar Pedido</h1>
+        </div>
+    )
+}
+export default Menu;
