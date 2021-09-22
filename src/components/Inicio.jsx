7a08757a148bb03715d02dad5330dd53ec9e73bc
TeresaOyarce <t.oyarce05@gmail.com>
2021-09-22 11:38:39 -0300

    router button ok

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import logo from '../img/logo2.1.png'
+import Menu from './Menu'
+import Ready from './ListOrder';
+import Cooking from './Cooking';
+import { useHistory } from "react-router-dom";
 
 export const Inicio = () => {
+    let history = useHistory();
     return (
         <div className='containerInicio'>
             <nav className='navBar'>
             <img src={logo} className='logo' />
             </nav>
             <div className='containerButton'>
-                <button className='buttonIndex' id='ButtonOne'>TOMAR PEDIDO</button>
-                <button className='buttonIndex' id='ButtonTwo'>PEDIDOS LISTOS</button>
-                <button className='buttonIndex' id='ButtonThree'>COCINA</button>
+                <button onClick={()=>{Menu(); history.push('/menu');}} className='buttonIndex' id='ButtonOne'>TOMAR PEDIDO</button>
+
+                <button onClick={()=>{Ready(); history.push('/ready');}}className='buttonIndex' id='ButtonTwo'>PEDIDOS LISTOS</button>
+
+                <button onClick={()=>{Cooking(); history.push('/cooking');}}className='buttonIndex' id='ButtonThree'>COCINA</button>
             </div>
            
         </div>
